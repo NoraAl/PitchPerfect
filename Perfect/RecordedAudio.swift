6fe808bbdf12d1f5c31fdf6da6b2ac42d2c4bbb1
NoraAl <nemseo@gmail.com>
2015-12-08 11:16:05 -0500

    all

---
+++ Perfect/RecordedAudio.swift
@@ -0,0 +1,17 @@
+//
+//  RecordedAudio.swift
+//  Perfect
+//
+//  Created by N on 9/29/15.
+//  Copyright (c) 2015 Nora. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordedAudio: NSObject{
+    var filePathURL: NSURL!
+    var title: String!
+    
+    
+}
